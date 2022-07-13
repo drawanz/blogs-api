8632d2383e2c294dac06e21a1fd5a96d96c4a347
Leonidas <leonidas.drawanz@gmail.com>
2022-07-13 19:43:08 -0300

    feat: adiciona a model de Category

---
+++ src/database/models/category.js
@@ -0,0 +1,17 @@
+'use strict';
+//JSdocs
+  /**
+   * @param {import('sequelize').Sequelize } sequelize 
+   * @param {import('sequelize').DataTypes} DataTypes 
+   */
+const createCategoryModel = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    id: DataTypes.INTEGER,
+    name: DataTypes.STRING,
+  }, {
+    tableName: 'Categories'
+  });
+  return Category;
+};
+
+module.exports = createCategoryModel
