51b03ba5e57140e4643de6319a5fba2d8aa1d27b
Leonidas <leonidas.drawanz@gmail.com>
2022-07-15 19:24:58 -0300

    chore: cria o getUser by ID na services

---
+++ src/services/getUserId.js
@@ -0,0 +1,20 @@
+const { User } = require('../database/models/index.js');
+const httpStatus = require('../helpers/httpStatusCode');
+
+const getUser = async (idUser) => {
+  const [user] = await User.findAll({
+    where: { id: `${idUser}` },
+    attributes: ['id', 'displayName', 'email', 'image'],
+  });
+
+  if (!user.dataValues) {
+    return {
+      status: httpStatus.BAD_REQUEST,
+      message: 'User does not exist',
+    };
+  }
+
+  return user.dataValues;
+};
+
+module.exports = getUser;
