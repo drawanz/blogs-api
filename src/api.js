1b53413795dc0a0997a333c5631e376f43bdaa67
Leonidas <leonidas.drawanz@gmail.com>
2022-07-16 10:06:02 -0300

    feat: implementando rota de add categorias

--- src/api.js
+++ src/api.js
@@ -6,6 +6,7 @@ const app = express();
 app.use(express.json());
 app.use('/login', routers.loginRoutes);
 app.use('/user', routers.userRoutes);
+app.use('/categories', routers.categoriesRoutes);
 
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
