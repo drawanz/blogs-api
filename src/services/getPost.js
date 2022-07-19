5880e7aab2803d7fe361a1a12ff93764d72eb427
Leonidas <leonidas.drawanz@gmail.com>
2022-07-18 21:49:10 -0300

    feat: adicionando a getPosts na camada services

---
+++ src/services/getPost.js
@@ -0,0 +1,13 @@
+const { BlogPost, User, Category } = require('../database/models/index.js');
+
+const getPost = async () => {
+  const posts = await BlogPost.findAll({ 
+    include: [
+      { model: User, as: 'user', attributes: { exclude: ['password'] } },
+      { model: Category, as: 'categories' },
+    ],
+  });
+  return posts;
+};
+
+module.exports = getPost;
--- src/services/index.js
+++ src/services/index.js
@@ -5,6 +5,7 @@ const getUserById = require('./getUserById');
 const postCategories = require('./postCategories');
 const getCategories = require('./getCategories');
 const postPost = require('./postPost');
+const getPost = require('./getPost');
 
 module.exports = {
   postLogin,
@@ -14,4 +15,5 @@ module.exports = {
   postCategories,
   getCategories,
   postPost,
+  getPost,
 };
